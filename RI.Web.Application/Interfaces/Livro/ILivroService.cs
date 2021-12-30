f8ce04111e0514e7ac302a1282842485209d7dc0
Yasmin Kruger <mimikruger58@gmail.com>
2021-12-30 12:46:04 -0300

    Implementação livros

---
+++ RI.Web.Application/Interfaces/Livro/ILivroService.cs
@@ -0,0 +1,9 @@
+﻿using RI.Web.Application.ViewModels.Livro;
+
+namespace RI.Web.Application.Interfaces.Livro
+{
+    public interface ILivroService
+    {
+        Task<LivroViewModel> ObterLivro(LivroViewModel livroViewModel);
+    }
+}
---
+++ RI.Web.Domain/Interfaces/Livro/ILivroRepository.cs
@@ -0,0 +1,9 @@
+﻿using RI.Web.Domain.Entities.Livro;
+
+namespace RI.Web.Domain.Interfaces.Livro
+{
+    public interface ILivroRepository
+    {
+        Task<LivroEntity> ObterLivro(LivroEntity livro);
+    }
+}
