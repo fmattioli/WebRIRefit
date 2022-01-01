7f975c9bafe7281f686d95cad5a6f30afa8e71b4
Felipe Santos - SPCM <felipe.santos@SPCM.COM.BR>
2022-01-01 18:28:07 -0300

    Novas implementações

---
+++ RI.Web.UI/wwwroot/js/livro/Livro.js
@@ -0,0 +1,76 @@
+﻿const nomeController = 'Livro';
+const url = `https://localhost:7054/api/v1`;
+
+function mostrarForm() {
+    if ($('#formularioLivro').css('display') == 'none') {
+        $('#formularioLivro').css('display', 'inline');
+        $('#showless').css('display', 'inline');
+        $('#add').css('display', 'none');
+    }
+    else {
+        $('#formularioLivro').css('display', 'none');
+        $('#add').css('display', 'inline');
+        $('#showless').css('display', 'none');
+    }
+}
+
+function editarLivro(livroId) {
+    if ($('#formularioLivro').css('display') == 'none') {
+        $('#formularioLivro').css('display', 'inline');
+        $('#showless').css('display', 'inline');
+        $('#add').css('display', 'none');
+    }
+
+    var endPoint = `${url}/${nomeController}/${livroId}`;
+
+    $.ajax({
+        method: "GET",
+        url: endPoint,
+        success: function (response) {
+            if (isNullOrEmpty(response.mensagemRetorno)) {
+                carretorTextoInput('descricao', response.result.descricaoLivro);
+                carretorTextoInput('sigla', response.result.sigla);
+                carretorTextoInput('sessao', response.result.sessao);
+                carretorTextoInput('ultimasequencia', response.result.ultimaSequenciaUtilizada);
+                carregarSelectPeloValor('controlaSeqLivro', response.result.controlaSequenciaDoLivro);
+                carregarSelectPeloValor('seqatozero', response.result.permiteSequenciaDoAtoZero);
+                carregarSelectPeloValor('tipolivro', response.result.livroTJ.idLivroTJ);
+                carregarSelectBooleano('enviarDOI', response.result.enviarDOI);
+                carregarSelectBooleano('apontarindisponibilidade', response.result.indisponibilidade);
+                carregarSelectBooleano('controlaseqato', response.result.controlaSequenciaDoAto);
+                carregarSelectBooleano('seqatoinicial', response.result.sequenciaInicialAtos);
+                carregarSelectBooleano('validar_reg_anterior', response.result.validarRegistroAnterior);
+                carregarSelectBooleano('permitegarantia', response.result.permiteDescreverGarantia);
+                carregarSelectBooleano('permitegarantia', response.result.permiteDescreverGarantia);
+                carregarSelectBooleano('transcricao', response.result.transcricao);
+                carregarSelectBooleano('enviarBDL', response.result.enviaBDL);
+                return;
+            }
+
+            M.toast({
+                html: response.msgErro,
+                classes: 'black darken-4 rounded',
+            });
+
+        },
+        error: function (xhr, textStatus, errorThrown) {
+            M.toast({
+                html: "Erro ao obter dados para edição do livro",
+                classes: 'carmesim darken-4 rounded',
+            });
+        }
+    })
+}
+
+function excluirLivro(idLivro) {
+    $('.modal').modal({
+        dismissble: true
+    });
+    $('#modal').modal('open');
+    $(".texto").text("Deseja realmente excluir?");
+
+}
+
+
+
+
---
+++ RI.Web.UI/wwwroot/js/shared/util.js
@@ -0,0 +1,34 @@
+﻿function isNullOrEmpty(s) {
+    return (s == null || s === "");
+}
+
+function carretorTextoInput(id, valueToSelect) {
+    let element = document.getElementById(id);
+    M.AutoInit();
+    element.value = valueToSelect;
+    $("#" + id).val();
+    $("#" + id).focus();
+}
+
+function carregarSelectPeloValor(id, valueToSelect) {
+    $('#' + id).val(parseInt(valueToSelect));
+    $('#' + id).formSelect();
+}
+
+
+function carregarSelectBooleano(id, valueToSelect) {
+    if (valueToSelect)
+        $('#' + id).val(parseInt(1));
+    else
+        $('#' + id).val(parseInt(0));
+    $('#' + id).formSelect();
+}
+
+function carregarSelectPeloNome(id, valueToSelect) {
+    $(`#${id} option:contains(${valueToSelect})`).attr('selected', true);
+    $('#' + id).formSelect();
+}
+
+
+
+
